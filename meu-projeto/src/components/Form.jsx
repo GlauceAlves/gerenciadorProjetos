aa4ce51bd5a98064359ab29450d6df1c999e0034
Glauce Alves <glaucemail@gmail.com>
2024-08-21 22:31:43 -0300

    .

--- meu-projeto/src/components/Form.jsx
+++ meu-projeto/src/components/Form.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'
+
 function Form() {
     function cadastrarUsuario(e) {
         e.preventDefault()
         console.log(`Cadastrou o usuario`)
     }
 
+    // sempre tem o nome do tributo e o que vai resgatar e o set name e o que vai mudar no use Satae e o valor que inicia quandoffor inicializado
+      
+    const [nome, setName] =useState()
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label htmlFor="nome">Nome: </label>
+                    <input type="text" id="nome" name="nome" placeholder="Digite seu nome" />
+                </div>
+                <div>
+                    <label htmlFor="passowrd">Senha: </label>
+                    <input type="password" id="senha" name="senha" placeholder="Digite sua senha" onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
